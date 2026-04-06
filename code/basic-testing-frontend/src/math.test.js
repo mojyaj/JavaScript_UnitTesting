633b3741d2c920320e1c2df0d96bfb369fb42596
mojyaj <yevaj72@gmail.com>
2026-04-06 05:38:41 -0500

    17. Writing a First Test

--- code/basic-testing-frontend/src/math.test.js
+++ code/basic-testing-frontend/src/math.test.js
@@ -1,3 +1,12 @@
-import { test } from 'vitest';
+import { it, expect } from 'vitest';
 
+import { add } from './math';
 
+it('should summarize all number values in an array', () => {
+
+    // Arrange and Act
+    const result = add([1,2,3]);
+
+    // Assert
+    expect(result).toBe(6);
+});
