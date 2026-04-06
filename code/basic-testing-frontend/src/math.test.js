a00e0417819caeec9063ec59cbddd2792f31785a
mojyaj <yevaj72@gmail.com>
2026-04-06 07:48:49 -0500

    22. Defining Behaviors & Fising Errors In Your Code

--- code/basic-testing-frontend/src/math.test.js
+++ code/basic-testing-frontend/src/math.test.js
@@ -17,3 +17,38 @@ it('should summarize all number values in an array', () => {
     );
     expect(result).toBe(expectedResult);
 });
+
+it('should yield NaN if at least one invalid number is provided', () => {
+   
+    // Arrange
+    const inputs = ['invalid', 1];  // a valid that can't be converted to a number
+
+    // Act
+    const result = add(inputs);
+
+    // Assert
+    expect(result).toBeNaN();
+
+});
+
+it('should yield a correct sum if an array of numeric string values is provided', () => {
+    // Arrange
+    const numbers = ['1', '2'];  // valid numeric strings
+
+    // Act
+    const result = add(numbers);
+
+    // Assert
+    const expectedResult = numbers.reduce(
+            (prevValue, curValue) => +prevValue + +curValue, // prefix variables with "+" 'Unary Plus' to convert operands to numbers before summing
+            0
+        );
+    expect(result).toBe(expectedResult);
+
+    /** 
+     * Failed test result:
+     *  - Expected   "3"
+     *  + Received   "012"
+     * 
+    */
+})
