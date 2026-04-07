2ab3cd0ae6b1cc6cff8c3d50e4faca513ebd8219
mojyaj <yevaj72@gmail.com>
2026-04-07 03:43:22 -0500

    24. Testing For Errors

--- code/basic-testing-frontend/src/math.test.js
+++ code/basic-testing-frontend/src/math.test.js
@@ -55,14 +55,24 @@ it('should yield a correct sum if an array of numeric string values is provided'
 
 it('should yield 0 if an empty array is provided', () => {
 
+    // Arrange
     const numbers = [];
-
+    
+    // Act
     const result =add(numbers);
 
+    // Assert
     expect(result).toBe(0);
 });
 
 it('should throw an error if no value is passed into the function', () => {
-    const result = add();
+    
+    // Act
+    const resultFn = () => {
+        // Wrap test function call in a function to be able to assert that it throws an error
+        add();
+    };
 
+    // Assert
+    expect(resultFn).toThrow();
 });
