25a96f05715bdc196c37ae37b2fd60273c21957e
mojyaj <yevaj72@gmail.com>
2026-04-11 08:57:21 -0500

    Staging Error - Previous 5 commits are missing some content

---
+++ code/basic-testing-nodejs-esmodules/src/math.test.js
@@ -0,0 +1,92 @@
+import { it, expect } from 'vitest';
+
+import { add } from './math';
+
+it('should summarize all number values in an array', () => {
+
+    // Arrange
+    const numbers = [1, 2, 3];
+
+    // Act
+    const result = add(numbers);
+
+    // Assert
+    const expectedResult = numbers.reduce(
+        (prevValue, curValue) => prevValue + curValue, 
+        0
+    );
+    expect(result).toBe(expectedResult);
+});
+
+it('should yield NaN if at least one invalid number is provided', () => {
+   
+    // Arrange
+    const inputs = ['invalid', 1];  // a valid that can't be converted to a number
+
+    // Act
+    const result = add(inputs);
+
+    // Assert
+    expect(result).toBeNaN();
+
+});
+
+it('should yield a correct sum if an array of numeric string values is provided', () => {
+    // Arrange
+    const numbers = ['1', '2'];  // valid numeric strings
+
+    // Act
+    const result = add(numbers);
+
+    // Assert
+    const expectedResult = numbers.reduce(
+            (prevValue, curValue) => +prevValue + +curValue, // prefix variables with "+" 'Unary Plus' to convert operands to numbers before summing
+            0
+        );
+    expect(result).toBe(expectedResult);
+
+    /** 
+     * Failed test result:
+     *  - Expected   "3"
+     *  + Received   "012"
+     * 
+    */
+})
+
+it('should yield 0 if an empty array is provided', () => {
+
+    // Arrange
+    const numbers = [];
+    
+    // Act
+    const result =add(numbers);
+
+    // Assert
+    expect(result).toBe(0);
+});
+
+it('should throw an error if no value is passed into the function', () => {
+    
+    // Act
+    const resultFn = () => {
+        // Wrap test function call in a function to be able to assert that it throws an error
+        add();
+    };
+
+    // Assert
+    expect(resultFn).toThrow();
+});
+
+it('should throw an error if provided with multiple arguments instead of an array', () => {
+
+    // Arrange
+    const num1 = 1;
+    const num2 = 2;
+
+    const resultFn = () => {
+        add(1,2); // multiple arguments instead of an array
+        // Trivia: TypeScript would have prevented this error at compile time by enforcing the correct type of the function's parameter
+    };
+
+    expect(resultFn).toThrow(/is not iterable/);
+});
