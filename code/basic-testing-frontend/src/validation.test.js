43a22bb70c81df88ab791f6225ece0117dab2d08
mojyaj <yevaj72@gmail.com>
2026-04-11 05:53:27 -0500

    30. Introducing Test Suites

--- code/basic-testing-frontend/src/validation.test.js
+++ code/basic-testing-frontend/src/validation.test.js
@@ -1,110 +1,120 @@
-import { it, expect } from 'vitest';
+import { describe, it, expect } from 'vitest';
 
 import { validateStringNotEmpty, validateNumber } from './util/validation.js'
 
-// validateStringNotEmpty() tests
-it('should throw an error message "Invalid input - must not be empty.", if argument is empty string', () => {
+
+describe('validateStringNotEmpty()', () => {
+
+    it('should throw an error message "Invalid input - must not be empty.", if argument is empty string', () => {
+        
+        // Arrange
+        const empStr = "";
+        const errMsg = 'Invalid input - must not be empty.';
+
+        // Act
+        const resultFn = () => validateStringNotEmpty(empStr);
+        
+        // Assert
+        expect(resultFn).toThrowError(errMsg);
     
-    // Arrange
-    const empStr = "";
-    const errMsg = 'Invalid input - must not be empty.';
+        // Tips:
+        //      Wrap the function inside expect()
+        //      const Vitest handle the error with .toThrowError()
+    });
 
-    // Act
-    const resultFn = () => validateStringNotEmpty(empStr);
-    
-    // Assert
-    expect(resultFn).toThrowError(errMsg);
-  
-    // Tips:
-    //      Wrap the function inside expect()
-    //      const Vitest handle the error with .toThrowError()
-});
+    it('should not throw if argument is a string literal', () => {
 
-it('should not throw if argument is a string literal', () => {
+        // Arrange
+        const str = "apple";
 
-    // Arrange
-    const str = "apple";
+        // Act
+        const  resultFn = () => validateStringNotEmpty(str);
 
-    // Act
-    const  resultFn = () => validateStringNotEmpty(str);
+        // Assert
+        expect(resultFn).not.toThrow();
+    });
 
-    // Assert
-    expect(resultFn).not.toThrow();
-});
+    it('should throw an error if argument is an object', () => {
+        
+        // Arrange
+        const obj = {};
 
-it('should throw an error if argument is an object', () => {
-    
-    // Arrange
-    const obj = {};
+        // Act
+        const resultFn =  () => validateStringNotEmpty(obj);
+        
+        // Assert
+        expect(resultFn).toThrow();
+    });
 
-    // Act
-    const resultFn =  () => validateStringNotEmpty(obj);
-    
-    // Assert
-    expect(resultFn).toThrow();
-});
+
+ });
+
 
 
 //------------------------------------
 // validateNumber() tests
 
-it('should throw if input is a string literal', () => {
+describe('validateNumber()', () => {  
+    it('should throw if input is a string literal', () => {
 
-    // Arrange
-    const input = '1';
+        // Arrange
+        const input = '1';
 
-    // Act 
-    const resultFn = () => validateNumber(input);
+        // Act 
+        const resultFn = () => validateNumber(input);
 
-    // Assert
-    expect(resultFn).toThrow();
+        // Assert
+        expect(resultFn).toThrow();
+    });
+
+    it('should not throw if input is a number', () => {
+
+        // Arrange
+        const input = 3;
+
+        // Act 
+        const resultFn = () => validateNumber(input); 
+        
+        // Assert
+        expect(resultFn).not.toThrow();
+    });
+
+    it('should throw error message, "Invalid number input", if input is NaN', () => {
+
+        // Arrange
+        const input = NaN;
+        const errMsg = 'Invalid number input';
+        
+        // Act 
+        const resultFn = () => validateNumber(input); 
+
+        // Assert
+        expect(resultFn).toThrowError(errMsg);
+    });
+
+    it('should throw if input is undefined', () => {
+
+        // Arrange
+        const input = undefined;
+
+        // Act
+        const validationFn = () => validateNumber(input);
+
+        //Assert
+        expect(validationFn).toThrow();
+    });
+
+    it('should throw an error if a non-numeric value is provided', () => {
+
+        // Arrange
+        const input = '1';
+
+        // Act
+        const validationFn = () => validateNumber(input);
+
+        // Assert
+        expect(validationFn).toThrow()
+    });
+
 });
 
-it('should not throw if input is a number', () => {
-
-    // Arrange
-    const input = 3;
-
-    // Act 
-    const resultFn = () => validateNumber(input); 
-    
-    // Assert
-    expect(resultFn).not.toThrow();
-});
-
-it('should throw error message, "Invalid number input", if input is NaN', () => {
-
-    // Arrange
-    const input = NaN;
-    const errMsg = 'Invalid number input';
-    
-    // Act 
-    const resultFn = () => validateNumber(input); 
-
-    // Assert
-    expect(resultFn).toThrowError(errMsg);
-});
-
-it('should throw if input is undefined', () => {
-
-    // Arrange
-    const input = undefined;
-
-    // Act
-    const validationFn = () => validateNumber(input);
-
-    //Assert
-    expect(validationFn).toThrow();
-});
-
-it('should throw an error if a non-numeric value is provided', () => {
-
-    // Arrange
-    const input = '1';
-
-    // Act
-    const validationFn = () => validateNumber(input);
-
-    // Assert
-    expect(validationFn).toThrow()
-});
