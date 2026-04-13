c0b931545196eff88c086ae45f1c3e1e8b48f75b
mojyaj <yevaj72@gmail.com>
2026-04-13 14:27:58 -0500

    56. Concurrent Tests

--- code/hooks/hooks.test.js
+++ code/hooks/hooks.test.js
@@ -24,7 +24,7 @@ afterEach( () => {
 });
 
 
-it('should update the email', () => {
+it.concurrent('should update the email', () => {
   
   const newTestEmail = 'test2@test.com';
 
@@ -33,24 +33,24 @@ it('should update the email', () => {
   expect(user.email).toBe(newTestEmail);
 });
 
-it('should have an email property', () => {
+it.concurrent('should have an email property', () => {
 
   expect(user).toHaveProperty('email');
 });
 
-it('should store the provided email value', () => {
+it.concurrent('should store the provided email value', () => {
 
   expect(user.email).toBe(testEmail);
 });
 
-it('should clear the email', () => {
+it.concurrent('should clear the email', () => {
   
   user.clearEmail();
 
   expect(user.email).toBe('');
 });
 
-it('should still have an email property after clearing the email', () => {
+it.concurrent('should still have an email property after clearing the email', () => {
   
   user.clearEmail();
 
