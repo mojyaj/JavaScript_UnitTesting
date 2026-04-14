f5b15b2bdde5fd02cfd7ea3d5a05a63e70157f1c
mojyaj <yevaj72@gmail.com>
2026-04-14 11:04:56 -0500

    64. Getting Started with Mocks & Automocking

--- code/section7-start-project/src/util/io.test.js
+++ code/section7-start-project/src/util/io.test.js
@@ -1,15 +1,26 @@
-import { it, expect } from 'vitest'
+import { it, expect,vi } from 'vitest'
+import { promises as fs } from 'fs';
 
 import  writeData from './io.js'
 
 // 'data.txt' file must exist before running tests
 
+vi.mock('fs');
+
 it('should execute the writeData method', () => {
 
     const testData = 'Test';
     const testFilename = 'test.txt';
-    return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
 
+    // Execute the mocked functions
+    writeData(testData, testFilename);
+
+    // Assert that the mocked 'writeFile()' was called inside of mocked 'writeData()'
+    expect(fs.writeFile).toBeCalled();
+
+    // Assertion using Spies
+    //return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+    
     // IMPORTANT TO KNOW
 	// 	.writeFile() (Promise version) behavior
 	// 		1) Resolves when the file is successfully written
