940c0861938d34afbed2082ec81f0b7d40db6884
mojyaj <yevaj72@gmail.com>
2026-04-13 20:37:56 -0500

    61. The Problem With Side Effects & Tests

---
+++ code/section7-start-project/src/util/io.test.js
@@ -0,0 +1,19 @@
+import { it, expect } from 'vitest'
+
+import  writeData from './io.js'
+
+// 'data.txt' file must exist before running tests
+
+it('should execute the writeData method', () => {
+
+    const testData = 'Test';
+    const testFilename = 'test.txt';
+    return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+
+    // IMPORTANT TO KNOW
+	// 	fs.writeFile() (Promise version) behavior
+	// 		1) Resolves when the file is successfully written
+	// 		2) Rejects if there is an error
+	// 		3) *** Resolves with undefined ***
+});
+
