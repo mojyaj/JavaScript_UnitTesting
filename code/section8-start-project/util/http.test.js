adc9b1cc2acf6f3b0014f170def0dafb61cdb565
mojyaj <yevaj72@gmail.com>
2026-04-16 18:17:04 -0500

    78. Test-specific Mocking Logic
    and 77. Mocking Frontend Libraries

--- code/section8-start-project/util/http.test.js
+++ code/section8-start-project/util/http.test.js
@@ -1,5 +1,6 @@
 import { it, vi, expect } from 'vitest'
 
+import { HttpError } from './errors.js'
 import { sendDataRequest } from './http.js'
 
 // Dummy Resolve Data
@@ -7,8 +8,11 @@ const testResponseData = {testKey: 'testData'};
 
 // Spy with added behavior that mimicks 'fetch()'
 const testFetch = vi.fn( (url, options) => {
-    
     return new Promise( (resolve, reject) => {
+        if (typeof options.body !== 'string') {
+            return reject('Not a string.');
+        }
+        
         const testResponse = {
             ok: true,
             json() { // mimick response.json()
@@ -29,4 +33,35 @@ it('should return a Promise containing any available response data', () => {
     const testData = {key: 'test'};
 
     return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
+});
+
+it('should convert the provided data to JSON before sending the request', async () => {
+    
+    const testData = {key: 'test'};
+
+    let errorMessage;
+
+    try {
+        sendDataRequest(testData);
+    } catch (error) {
+        errorMessage = error;
+    }
+
+    /*
+        // This line is easy to mis-read: The Promise should NOT reject with 'Not a string.'
+        return expect(sendDataRequest(testData)).not.rejects.toBe('Not a string.');
+
+        1) sendDataRequest(testData) returns a Promise
+        2) .rejects expects the Promise to reject (fail)
+        3) .not.rejects expects the Promise to NOT reject
+        4) .toBe('Not a string.'), this part only applies IF it rejects
+        The Promise should NOT reject with 'Not a string.'
+    */
+});
+
+it('should throw an HttpError in case of non-ok responses', () => {
+
+    const testData = {key: 'test'};
+    
+    return expect(sendDataRequest(testData)).rejects.toBeInstanceOf(HttpError);
 });
